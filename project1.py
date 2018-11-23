68007adf04b921492a2012e3d08097d7c6ba95d1
Blue Owl <jrod188@gmail.com>
2018-11-23 11:18:18 -0600

    refresh py

--- project1.py
+++ project1.py
@@ -54,7 +54,9 @@ market.drop(columns =["Adj Close"], inplace = True)
 market.drop(columns =["Volume"], inplace = True) 
 market.drop(columns =["Low"], inplace = True) 
 market.drop(columns =["High"], inplace = True) 
+market['Date']=market['Month'].astype(str)+'/'+market['Day']+'/'+market['Year']
 market.drop(columns =["Day"], inplace = True) 
+
 
 # Export the results to text file
 market.to_csv('market2.csv', index = False)
@@ -62,7 +64,8 @@ market.to_csv('market2.csv', index = False)
 #initalize market types
 high_profit_market = market
 low_profit_market = market
-
+market2 = market
+market3 = market
 
 #df display 
 #print (market) 
@@ -123,6 +126,35 @@ def label_may_oct_month (row):
       return np.NaN  
    return 'Other'
 
+def label_month (row):
+   if row['Month'] == '01' :
+      return 'Nov-Apr'
+   if row['Month'] == '02' :
+      return 'Nov-Apr'
+   if row['Month'] == '03' :
+      return 'Nov-Apr'
+   if row['Month'] == '04' :
+      return 'Nov-Apr'
+   if row['Month'] == '05' :
+      return 'May-Oct'
+   if row['Month'] == '06' :
+      return 'May-Oct'
+   if row['Month'] == '07' :
+      return 'May-Oct'
+   if row['Month'] == '08' :
+      return 'May-Oct'
+   if row['Month'] == '09' :
+      return 'May-Oct'
+   if row['Month'] == '10' :
+      return 'May-Oct'
+   if row['Month'] == '11' :
+      return 'Nov-Apr'
+   if row['Month'] == '12' :
+      return 'Nov-Apr' 
+   return 'Other'
+
+market['P_Season'] = market.apply (lambda row: label_month (row),axis=1)
+
 #create label column and drop NaN
 high_profit_market['Season'] = high_profit_market.apply (lambda row: label_nov_apr_month (row),axis=1)
 high_profit_market = high_profit_market.dropna(axis = 0, how ='any') 
@@ -130,15 +162,27 @@ high_profit_market = high_profit_market.reset_index(drop = True)
 high_profit_market.drop(market.index[:1], inplace=True)
 high_profit_market = high_profit_market.reset_index(drop = True)
 
-high_profit_market ['return'] = high_profit_market (lambda row: 'Open'/'Close'.shift(1))
+#high_profit_market ['return'] = high_profit_market (lambda row: 'Open'/'Close'.shift(1))
 
 low_profit_market['Season'] = low_profit_market.apply (lambda row: label_may_oct_month (row),axis=1)
 low_profit_market = low_profit_market.dropna(axis = 0, how ='any') 
 low_profit_market = low_profit_market.reset_index(drop = True)
 
+low_profit_market['Return'] = low_profit_market.Open.pct_change(1)
+
+
+market2.drop(market.columns[[5]], axis=1, inplace=True)
+market2 = market.dropna(axis = 0, how ='any') 
+market2 = market.reset_index(drop = True)
+
+#rng = pd.date_range('5/1/1950', periods=180, freq='M')
+#market2 = market2.set_index('Date')
+#market2 ['Return'] = market2['Open'] - market2['Open'].shift(6)/ market2
+
 #view data
-print (high_profit_market)
-print (low_profit_market)
+print (high_profit_market.head())
+print (low_profit_market.head())
+print (market2.head())
 
 # Track various financial parameters
 
